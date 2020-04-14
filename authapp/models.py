2b2f86700b738bcff3f069bf59979de6ffad88ad
memetics19 <shreeda.bhat@yahoo.com>
2020-04-14 21:30:56 +0530

    update models

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class User(Model):
+    email = models.EmailField(max_length=100)
+    username = models.CharField(max_length=100)
